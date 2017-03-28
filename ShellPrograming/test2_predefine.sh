8d5c94bb33bfb93406ea53acec65ecf9352df012
Jathy <1302552664@qq.com>
2017-03-28 15:33:34 +0800

    the common predefine variable in SHELL

---
+++ ShellPrograming/test2_predefine.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo "This is the name of pragrame: $0"
+echo "This is the number of arguments: $#"
+echo "This is all arguments: $*"
+echo "The current PID: $$"
+echo "the return state after command execute: $?"
+echo "the last PID in background programming: $!"
